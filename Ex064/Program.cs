fca342925a672e01e18f2f1205ee45f5e3e551df
Danil <danil-8951@mail.ru>
2022-11-26 21:49:40 +0500

    Показывает все натуральные числа от N до 1

---
+++ Ex064/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64: Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+void NumberCounter(int userNumber)
+{
+    if (userNumber < 0) Console.Write($"{userNumber} не натуральное число");
+    if (userNumber == 0) return;
+    Console.Write("{0,4}", userNumber);
+    NumberCounter (userNumber - 1);
+}
+
+int userNumber = GetNumber("Введите число: ");
+Console.WriteLine();
+NumberCounter(userNumber);
